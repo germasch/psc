4c699f0a1b8380b85520a67104daf13497f8f4d2
Kai Germaschewski <kai.germaschewski@unh.edu>
2019-02-08 23:45:01 -0500

    TMP: adios2 related testing

--- src/libpsc/tests/test_adios2.cxx
+++ src/libpsc/tests/test_adios2.cxx
@@ -1,6 +1,42 @@
 
 #include <adios2.h>
 
+void test1()
+{
+  using T = char;
+
+  adios2::ADIOS ad(MPI_COMM_WORLD, adios2::DebugON);
+
+  {
+    adios2::IO io_writer = ad.DeclareIO("writer");
+    auto var = io_writer.DefineVariable<T>("var");
+    auto writer = io_writer.Open("test.bp", adios2::Mode::Write);
+    
+    T test = 99;
+    writer.Put(var, test);
+    writer.Close();
+  }
+
+  {
+    adios2::IO io_reader = ad.DeclareIO("reader");
+    auto reader = io_reader.Open("test.bp", adios2::Mode::Read);
+    auto var = io_reader.InquireVariable<T>("var");
+    assert(static_cast<bool>(var));
+    
+    T test;
+    reader.Get(var, test);
+    reader.Close();
+
+    assert(test == 99);
+  }
+}
+
+template<typename T>
+void print_type(const char* name)
+{
+  printf("%s: %s %s\n", name, typeid(T).name(), typeid(typename adios2::TypeInfo<T>::IOType).name());
+}
+
 int main(int argc, char** argv)
 {
   MPI_Init(&argc, &argv);
@@ -8,7 +44,18 @@ int main(int argc, char** argv)
   int rank, size;
   MPI_Comm_rank(comm, &rank);
   MPI_Comm_size(comm, &size);
+
+  print_type<char>("char");
+  print_type<signed char>("signed char");
+  print_type<unsigned char>("unsigned char");
   
+  print_type<unsigned long>("unsigned long");
+  print_type<unsigned long long>("unsigned long long");
+  print_type<size_t>("size_t");
+  print_type<uint64_t>("uint64_t");
+  test1();
+  return 0;
+
   //auto configfile = "adios2.xml";
   adios2::ADIOS ad(comm, adios2::DebugON);
 
