ca6ec573ef19323124612e402337fe5194041e67
Kai Germaschewski <kai.germaschewski@unh.edu>
2019-02-28 22:51:23 -0500

    adios2, exp wip

--- src/libpsc/tests/test_adios2.cxx
+++ src/libpsc/tests/test_adios2.cxx
@@ -1,16 +1,20 @@
 
 #include <adios2.h>
 
+#include <iostream>
+#include <utility>
+#include <cmath>
+
 void test1()
 {
-  using T = char;
+  using T = unsigned long;
 
-  adios2::ADIOS ad(MPI_COMM_WORLD, adios2::DebugON);
+  adios2::ADIOS ad("adios.xml", MPI_COMM_WORLD, adios2::DebugON);
 
   {
-    adios2::IO io_writer = ad.DeclareIO("writer");
+    adios2::IO io_writer = ad.DeclareIO("io_writer");
     auto var = io_writer.DefineVariable<T>("var");
-    auto writer = io_writer.Open("test.bp", adios2::Mode::Write);
+    auto writer = io_writer.Open("test", adios2::Mode::Write);
     
     T test = 99;
     writer.Put(var, test);
@@ -18,8 +22,8 @@ void test1()
   }
 
   {
-    adios2::IO io_reader = ad.DeclareIO("reader");
-    auto reader = io_reader.Open("test.bp", adios2::Mode::Read);
+    adios2::IO io_reader = ad.DeclareIO("io_reader");
+    auto reader = io_reader.Open("test", adios2::Mode::Read);
     auto var = io_reader.InquireVariable<T>("var");
     assert(static_cast<bool>(var));
     
@@ -31,11 +35,233 @@ void test1()
   }
 }
 
-template<typename T>
-void print_type(const char* name)
+void test2()
 {
-  printf("%s: %s %s\n", name, typeid(T).name(), typeid(typename adios2::TypeInfo<T>::IOType).name());
+  using T = int32_t;
+
+  adios2::ADIOS ad("adios.xml", MPI_COMM_WORLD, adios2::DebugON);
+
+  {
+    adios2::IO io_writer = ad.DeclareIO("io_writer");
+    auto var = io_writer.DefineVariable<T>("var");
+    auto var2 = io_writer.DefineVariable<T>("var2");
+    auto writer = io_writer.Open("test", adios2::Mode::Write);
+    io_writer.RemoveVariable("var");
+    auto var3 = io_writer.DefineVariable<T>("var3");
+    std::cout << "var2 name " << var2.Name() << std::endl;
+    std::cout << "var3 name " << var3.Name() << std::endl;
+    
+    T test = 99;
+    writer.Put(var3, test);
+    writer.Close();
+  }
 }
+
+struct Base
+{
+  std::string type;
+};
+
+struct A : Base
+{
+  int val = 99;
+};
+
+struct B : Base
+{
+  std::string val = "hi";
+};
+
+struct Print
+{
+  template <typename T>
+  void operator()(T& t, const std::string& msg)
+  {
+    std::cout << msg << t.val << std::endl;
+  }
+};
+
+template <typename Visitor, typename... Args>
+void visit(Visitor&& v, const Base& p, Args&&... args)
+{
+  if (p.type == "A") {
+    v(reinterpret_cast<const A&>(p), std::forward<Args>(args)...);
+  } else if (p.type == "B") {
+    v(reinterpret_cast<const B&>(p), std::forward<Args>(args)...);
+  }
+};
+
+void test3()
+{
+  A a;
+  a.type = "A";
+
+  B b;
+  b.type = "B";
+
+  Base& pa = a;
+  visit(Print{}, pa, "pa = ");
+  Base& pb = b;
+  visit(Print{}, pb, "pb = ");
+}
+
+template<typename T>
+struct Test
+{
+  void print(T t)
+  {
+    std::cout << t;
+  }
+
+  template<typename U = T>
+  typename std::enable_if<std::is_arithmetic<U>::value, T>::type func(T t)
+  {
+    return sqrtf(t);
+  }
+};
+
+template class Test<int>;
+template class Test<float>;
+template class Test<std::string>;
+
+// ----------------------------------------------------------------------
+
+#include "MetaStuff/include/Meta.h"
+#include "MetaStuff/example/Person.h"
+
+template <typename Class, typename F,
+	  typename = typename std::enable_if<meta::isRegistered<Class>()>::type>
+void doForAllMembers(F&& f);
+
+// version for non-registered classes (to generate less template stuff)
+template <typename Class, typename F,
+	  typename = typename std::enable_if_t<!meta::isRegistered<Class>()>::type,
+	  typename = void>
+void doForAllMembers(F&& f);
+
+template <typename Class, typename F, typename>
+void doForAllMembers(F&& f)
+{
+  meta::detail::for_tuple(std::forward<F>(f), meta::getMembers<Class>());
+}
+
+using Tuple = std::tuple<std::string, int>;
+
+template <typename F>
+void _doForAllMembers(Tuple t, F&& f)
+{
+  f(std::get<0>(t));
+  f(std::get<1>(t));
+}
+
+template<typename T>
+void print(T val)
+{
+  std::cout << "Val " << val << std::endl;
+}
+
+void test4()
+{
+  Person person;
+  person.age = 40;
+  person.name = "Me";
+  doForAllMembers<Person>(
+    [&person](const auto& member)
+    {
+      using MemberT = meta::get_member_type<decltype(member)>;
+      std::cout << "* " << member.getName() <<
+        ", value = " << member.get(person) <<
+        ", type = " << typeid(MemberT).name() << '\n';
+    });
+
+  Tuple t = {"hi", 99};
+
+  _doForAllMembers(
+		   t,
+		   [](const auto& val)
+		   {
+		     print(val);
+		     std::cout << "* " << val << std::endl;
+		   });
+  
+}
+
+// ======================================================================
+
+// mp_list
+
+template<class... T> struct mp_list {};
+
+// mp_rename
+
+template<class A, template<class...> class B> struct mp_rename_impl;
+
+template<template<class...> class A, class... T, template<class...> class B>
+struct mp_rename_impl<A<T...>, B>
+{
+  using type = B<T...>;
+};
+
+template<class A, template<class...> class B>
+using mp_rename = typename mp_rename_impl<A, B>::type;
+
+// mp_size
+
+template<class... T> using mp_length = std::integral_constant<std::size_t, sizeof...(T)>;
+
+template<class L> using mp_size = mp_rename<L, mp_length>;
+
+// mp_push_front
+
+template<class L, class... T> struct mp_push_front_impl;
+
+template<template<class...> class L, class... U, class... T>
+    struct mp_push_front_impl<L<U...>, T...>
+{
+    using type = L<T..., U...>;
+};
+
+template<class L, class... T>
+    using mp_push_front = typename mp_push_front_impl<L, T...>::type;
+
+// add_pointer
+
+template<class T> using add_pointer = T*;
+
+// mp_transform
+
+template<template<class...> class F, class L> struct mp_transform_impl;
+
+template<template<class...> class F, class L>
+    using mp_transform = typename mp_transform_impl<F, L>::type;
+
+template<template<class...> class F, template<class...> class L, class... T>
+    struct mp_transform_impl<F, L<T...>>
+{
+    using type = L<F<T>...>;
+};
+
+//
+
+template <class T>
+struct Variable
+{};
+
+using MyTypes = mp_list<double, std::string>;
+
+using MyTuple = mp_rename<MyTypes, std::tuple>;
+
+using MyVars = mp_transform<Variable, MyTuple>;
+
+void test5()
+{
+  MyTuple vars = { 1.5, "Hi" };
+  std::cout << "size = " << mp_size<MyTypes>::value << std::endl;
+  using T = mp_push_front<MyTypes, float>;
+  std::cout << typeid(T).name() << std::endl;
+  std::cout << typeid(MyVars).name() << std::endl;
+}
+
 
 int main(int argc, char** argv)
 {
@@ -45,15 +271,7 @@ int main(int argc, char** argv)
   MPI_Comm_rank(comm, &rank);
   MPI_Comm_size(comm, &size);
 
-  print_type<char>("char");
-  print_type<signed char>("signed char");
-  print_type<unsigned char>("unsigned char");
-  
-  print_type<unsigned long>("unsigned long");
-  print_type<unsigned long long>("unsigned long long");
-  print_type<size_t>("size_t");
-  print_type<uint64_t>("uint64_t");
-  test1();
+  test5();
   return 0;
 
   //auto configfile = "adios2.xml";
--- src/libpsc/tests/test_adios2_local_single_value.cxx
+++ src/libpsc/tests/test_adios2_local_single_value.cxx
@@ -1,5 +1,6 @@
 
 #include <adios2.h>
+#include <iostream>
 
 #define mprintf(fmt...) do { int __rank; MPI_Comm_rank(MPI_COMM_WORLD, &__rank); { printf("[%d] ", __rank); printf(fmt); } } while(0)
 
@@ -15,7 +16,11 @@ int main(int argc, char** argv)
 
   {
     auto io_witer = ad.DeclareIO("io_writer");
-    auto varSize = io_witer.DefineVariable<int>("Size");
+    auto varTest = io_witer.DefineVariable<int>("TestVar");
+    std::cout << "varTest = " << varTest << std::endl;
+    std::cout << "ShapeID = " << varTest.ShapeID() << std::endl;
+    auto shape_id = varTest.ShapeID();
+    auto varSize = varTest;
     auto varRankLocal = io_witer.DefineVariable<int>("RankLocal", {adios2::LocalValueDim});
     
     // --- writing
@@ -38,9 +43,9 @@ int main(int argc, char** argv)
     // --- reading back
     auto reader = io_reader.Open("test.bp", adios2::Mode::Read);
 
-    auto varSize = io_reader.InquireVariable<int>("Size");
+    auto varSize = io_reader.InquireVariable<int>("TestVar");
     auto varRankLocal = io_reader.InquireVariable<int>("RankLocal");
-    // auto shape_id = varRankLocal.ShapeID();
+    auto shape_id = varRankLocal.ShapeID();
     // mprintf("shapeId %d LocalArray %d LocalValue %d GlobalArray %d\n", shape_id, adios2::ShapeID::LocalArray,
     // 	    adios2::ShapeID::LocalValue, adios2::ShapeID::GlobalArray);
 
