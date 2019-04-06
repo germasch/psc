2b1f0e0f929edcced4477e2fd5f308772ad8e98b
Kai Germaschewski <kai.germaschewski@unh.edu>
2019-04-05 22:26:35 -0400

    io: more tests

--- src/libpsc/tests/test_kg_io.cxx
+++ src/libpsc/tests/test_kg_io.cxx
@@ -24,6 +24,83 @@ TEST(KgIo, WriteRead)
   }
 }
 
+TEST(KgIo, WriteReadAttr)
+{
+  auto io = kg::io::IO(MPI_COMM_WORLD);
+
+  {
+    auto writer = io.open("test.bp", kg::io::Mode::Write);
+    auto attr_double = kg::io::Attribute<double>{"attr_double", writer};
+    writer.put(attr_double, 99.);
+    writer.close();
+  }
+
+  {
+    auto reader = io.open("test.bp", kg::io::Mode::Read);
+    auto attr_double = kg::io::Attribute<double>{"attr_double", reader};
+    double dbl;
+    reader.get(attr_double, dbl);
+    reader.close();
+    EXPECT_EQ(dbl, 99.);
+  }
+}
+
+struct Custom
+{
+  int i;
+  double d;
+};
+
+template <>
+class kg::io::Variable<Custom>
+{
+public:
+  Variable(const std::string& name, kg::io::Engine& engine)
+    : attr_i_{name + ".i", engine}, var_d_{name + ".d", engine}
+  {}
+
+  void put(kg::io::Engine& writer, const Custom& c,
+           const kg::io::Mode launch = kg::io::Mode::Deferred)
+  {
+    writer.put(attr_i_, c.i, launch);
+    writer.put(var_d_, c.d, launch);
+  }
+
+  void get(kg::io::Engine& reader, Custom& c,
+           const kg::io::Mode launch = kg::io::Mode::Deferred)
+  {
+    reader.get(attr_i_, c.i, launch);
+    reader.get(var_d_, c.d, launch);
+  }
+
+private:
+  kg::io::Attribute<int> attr_i_;
+  kg::io::VariableLocalSingleValue<double> var_d_;
+};
+
+TEST(KgIo, WriteReadCustom)
+{
+  auto io = kg::io::IO(MPI_COMM_WORLD);
+
+  {
+    auto writer = io.open("test.bp", kg::io::Mode::Write);
+    auto var_custom = kg::io::Variable<Custom>{"var_custom", writer};
+    auto c = Custom{3, 99.};
+    writer.put(var_custom, c);
+    writer.close();
+  }
+
+  {
+    auto reader = io.open("test.bp", kg::io::Mode::Read);
+    auto var_custom = kg::io::Variable<Custom>{"var_custom", reader};
+    auto c = Custom{};
+    reader.get(var_custom, c);
+    reader.close();
+    EXPECT_EQ(c.i, 3);
+    EXPECT_EQ(c.d, 99.);
+  }
+}
+
 // ======================================================================
 // main
 
