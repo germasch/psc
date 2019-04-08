9d3bfa2ea0102fb86bbb411e1d2fcb6f158616f0
Kai Germaschewski <kai.germaschewski@unh.edu>
2019-04-08 16:05:27 -0400

    mfields: rm setValues test
    not used, not pretty

--- src/libpsc/tests/test_mfields.cxx
+++ src/libpsc/tests/test_mfields.cxx
@@ -22,38 +22,6 @@ static Grid_t make_grid()
   return Grid_t{domain, bc, kinds, norm, dt};
 }
 
-// FIXME, consolidate / replace by more generic coord-dependent one
-template<typename Mfields, typename Set>
-void setValues(Mfields& mflds, Set set)
-{
-  for (int p = 0; p < mflds.n_patches(); p++) {
-    auto F = mflds[p];
-    F(EX, 0,0,0) = set(EX);
-    F(EX, 0,1,0) = set(EX);
-    F(EX, 0,0,1) = set(EX);
-    F(EX, 0,1,1) = set(EX);
-
-    F(EY, 0,0,0) = set(EY);
-    F(EY, 0,0,1) = set(EY);
-    F(EY, 1,0,0) = set(EY);
-    F(EY, 1,0,1) = set(EY);
-    
-    F(EZ, 0,0,0) = set(EZ);
-    F(EZ, 1,0,0) = set(EZ);
-    F(EZ, 0,1,0) = set(EZ);
-    F(EZ, 1,1,0) = set(EZ);
-
-    F(HX, 0,0,0) = set(HX);
-    F(HX, 1,0,0) = set(HX);
-
-    F(HY, 0,0,0) = set(HY);
-    F(HY, 0,1,0) = set(HY);
-
-    F(HZ, 0,0,0) = set(HZ);
-    F(HZ, 0,0,1) = set(HZ);
-  }
-}
-  
 template <typename T>
 class MfieldsTest : public ::testing::Test
 {};
@@ -88,26 +56,6 @@ TYPED_TEST(MfieldsTest, Access)
   mflds[0](0, 1, 1, 1) = 99.;
 
   EXPECT_EQ(mflds[0](0, 1, 1, 1), 99.);
-}
-
-TYPED_TEST(MfieldsTest, Set)
-{
-  using Mfields = TypeParam;
-
-  auto grid = make_grid();
-  auto mflds = Mfields{grid, NR_FIELDS, Int3{ 1, 1, 1 }};
-
-  setValues(mflds, [](int m) -> typename Mfields::real_t {
-      switch(m) {
-      case EX: return 1.;
-      case EY: return 2.;
-      case EZ: return 3.;
-      default: return 0.;
-      }
-    });
-
-  auto F = mflds[0];
-  EXPECT_EQ(F(EY, 0, 0, 0), 2.);
 }
 
 TYPED_TEST(MfieldsTest, ZeroComp)
