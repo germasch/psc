70595c024aa1e1f2bc266d9a1de3a3c8653f1a46
Kai Germaschewski <kai.germaschewski@unh.edu>
2019-05-01 11:29:54 -0400

    Variable<MfieldsC> -> fields3d.inl

---
+++ src/include/fields3d.inl
@@ -0,0 +1,89 @@
+
+#include "kg/io.h"
+
+#include "psc_fields_c.h"
+
+// ======================================================================
+// Variable<Mfields>
+
+template <>
+class kg::io::Variable<MfieldsC>
+{
+  using DataType = MfieldsC::real_t;
+
+public:
+  using value_type = MfieldsC;
+
+  Variable(const std::string& name, kg::io::Engine& engine) {}
+
+  void put(kg::io::Engine& writer, const value_type& mflds,
+           const kg::io::Mode launch = kg::io::Mode::Deferred)
+  {
+    const Grid_t& grid = mflds.grid();
+
+    writer.putAttribute("ib", mflds.ib, launch);
+    writer.putAttribute("im", mflds.im, launch);
+
+    auto& gdims = grid.domain.gdims;
+    size_t n_comps = mflds.n_comps();
+    auto shape = kg::io::Dims{n_comps, static_cast<size_t>(gdims[2]),
+                              static_cast<size_t>(gdims[1]),
+                              static_cast<size_t>(gdims[0])};
+    auto var = writer._defineVariable<DataType>(writer.prefix(), shape);
+    for (int p = 0; p < grid.n_patches(); p++) {
+      auto& patch = grid.patches[p];
+      auto start = kg::io::Dims{0, static_cast<size_t>(patch.off[2]),
+                                static_cast<size_t>(patch.off[1]),
+                                static_cast<size_t>(patch.off[0])};
+      auto count = kg::io::Dims{n_comps, static_cast<size_t>(grid.ldims[2]),
+                                static_cast<size_t>(grid.ldims[1]),
+                                static_cast<size_t>(grid.ldims[0])};
+      auto ib = kg::io::Dims{0, static_cast<size_t>(-mflds.ib[2]),
+                             static_cast<size_t>(-mflds.ib[1]),
+                             static_cast<size_t>(-mflds.ib[0])};
+      auto im = kg::io::Dims{n_comps, static_cast<size_t>(mflds.im[2]),
+                             static_cast<size_t>(mflds.im[1]),
+                             static_cast<size_t>(mflds.im[0])};
+      var.setSelection({start, count});
+      var.setMemorySelection({ib, im});
+      var.put(writer, mflds.data[p].get());
+    }
+  }
+
+  void get(kg::io::Engine& reader, value_type& mflds,
+           const kg::io::Mode launch = kg::io::Mode::Deferred)
+  {
+    const Grid_t& grid = mflds.grid();
+
+    // FIXME, should just check for consistency? (# ghosts might differ, too)
+    // reader.getAttribute("ib", mflds.ib, launch);
+    // reader.getAttribute("im", mflds.im, launch);
+
+    auto& gdims = grid.domain.gdims;
+    size_t n_comps = mflds.n_comps();
+    auto shape = kg::io::Dims{n_comps, static_cast<size_t>(gdims[2]),
+                              static_cast<size_t>(gdims[1]),
+                              static_cast<size_t>(gdims[0])};
+    auto var = reader._defineVariable<DataType>(reader.prefix());
+    assert(var.shape() == shape);
+    for (int p = 0; p < grid.n_patches(); p++) {
+      auto& patch = grid.patches[p];
+      auto start = kg::io::Dims{0, static_cast<size_t>(patch.off[2]),
+                                static_cast<size_t>(patch.off[1]),
+                                static_cast<size_t>(patch.off[0])};
+      auto count = kg::io::Dims{n_comps, static_cast<size_t>(grid.ldims[2]),
+                                static_cast<size_t>(grid.ldims[1]),
+                                static_cast<size_t>(grid.ldims[0])};
+      auto ib = kg::io::Dims{0, static_cast<size_t>(-mflds.ib[2]),
+                             static_cast<size_t>(-mflds.ib[1]),
+                             static_cast<size_t>(-mflds.ib[0])};
+      auto im = kg::io::Dims{n_comps, static_cast<size_t>(mflds.im[2]),
+                             static_cast<size_t>(mflds.im[1]),
+                             static_cast<size_t>(mflds.im[0])};
+      var.setSelection({start, count});
+      var.setMemorySelection({ib, im});
+      var.get(reader, mflds.data[p].get());
+    }
+  }
+};
+
--- src/libpsc/tests/test_mfields_io.cxx
+++ src/libpsc/tests/test_mfields_io.cxx
@@ -17,97 +17,14 @@
 #endif
 
 #include "kg/io.h"
-
-// ======================================================================
-// Variable<Mfields>
-
-template <>
-class kg::io::Variable<MfieldsC>
-{
-  using DataType = MfieldsC::real_t;
-
-public:
-  using value_type = MfieldsC;
-
-  Variable(const std::string& name, kg::io::Engine& engine) {}
-
-  void put(kg::io::Engine& writer, const value_type& mflds,
-           const kg::io::Mode launch = kg::io::Mode::Deferred)
-  {
-    const Grid_t& grid = mflds.grid();
-
-    writer.putAttribute("ib", mflds.ib, launch);
-    writer.putAttribute("im", mflds.im, launch);
-
-    auto& gdims = grid.domain.gdims;
-    size_t n_comps = mflds.n_comps();
-    auto shape = kg::io::Dims{n_comps, static_cast<size_t>(gdims[2]),
-                              static_cast<size_t>(gdims[1]),
-                              static_cast<size_t>(gdims[0])};
-    auto var = writer._defineVariable<DataType>(writer.prefix(), shape);
-    for (int p = 0; p < grid.n_patches(); p++) {
-      auto& patch = grid.patches[p];
-      auto start = kg::io::Dims{0, static_cast<size_t>(patch.off[2]),
-                                static_cast<size_t>(patch.off[1]),
-                                static_cast<size_t>(patch.off[0])};
-      auto count = kg::io::Dims{n_comps, static_cast<size_t>(grid.ldims[2]),
-                                static_cast<size_t>(grid.ldims[1]),
-                                static_cast<size_t>(grid.ldims[0])};
-      auto ib = kg::io::Dims{0, static_cast<size_t>(-mflds.ib[2]),
-                             static_cast<size_t>(-mflds.ib[1]),
-                             static_cast<size_t>(-mflds.ib[0])};
-      auto im = kg::io::Dims{n_comps, static_cast<size_t>(mflds.im[2]),
-                             static_cast<size_t>(mflds.im[1]),
-                             static_cast<size_t>(mflds.im[0])};
-      var.setSelection({start, count});
-      var.setMemorySelection({ib, im});
-      var.put(writer, mflds.data[p].get());
-    }
-  }
-
-  void get(kg::io::Engine& reader, value_type& mflds,
-           const kg::io::Mode launch = kg::io::Mode::Deferred)
-  {
-    const Grid_t& grid = mflds.grid();
-
-    // FIXME, should just check for consistency? (# ghosts might differ, too)
-    // reader.getAttribute("ib", mflds.ib, launch);
-    // reader.getAttribute("im", mflds.im, launch);
-
-    auto& gdims = grid.domain.gdims;
-    size_t n_comps = mflds.n_comps();
-    auto shape = kg::io::Dims{n_comps, static_cast<size_t>(gdims[2]),
-                              static_cast<size_t>(gdims[1]),
-                              static_cast<size_t>(gdims[0])};
-    auto var = reader._defineVariable<DataType>(reader.prefix());
-    assert(var.shape() == shape);
-    for (int p = 0; p < grid.n_patches(); p++) {
-      auto& patch = grid.patches[p];
-      auto start = kg::io::Dims{0, static_cast<size_t>(patch.off[2]),
-                                static_cast<size_t>(patch.off[1]),
-                                static_cast<size_t>(patch.off[0])};
-      auto count = kg::io::Dims{n_comps, static_cast<size_t>(grid.ldims[2]),
-                                static_cast<size_t>(grid.ldims[1]),
-                                static_cast<size_t>(grid.ldims[0])};
-      auto ib = kg::io::Dims{0, static_cast<size_t>(-mflds.ib[2]),
-                             static_cast<size_t>(-mflds.ib[1]),
-                             static_cast<size_t>(-mflds.ib[0])};
-      auto im = kg::io::Dims{n_comps, static_cast<size_t>(mflds.im[2]),
-                             static_cast<size_t>(mflds.im[1]),
-                             static_cast<size_t>(mflds.im[0])};
-      var.setSelection({start, count});
-      var.setMemorySelection({ib, im});
-      var.get(reader, mflds.data[p].get());
-    }
-  }
-};
+#include "fields3d.inl"
 
 #include "psc.h" // FIXME, just for EX etc
 
 static Grid_t make_grid()
 {
   auto domain =
-    Grid_t::Domain{{8, 4, 2}, {80., 40., 20.}, {-40., -20., 0.}, {2, 2, 1}};
+    Grid_t::Domain{{8, 4, 2}, {80., 40., 20.}, {-40., -20., 0.}, {2, 1, 1}};
   auto bc = GridBc{};
   auto kinds = Grid_t::Kinds{};
   auto norm = Grid_t::Normalization{};
@@ -235,13 +152,15 @@ TYPED_TEST(MfieldsTest, WriteReadWithGhosts)
   for (int p = 0; p < mflds.n_patches(); ++p) {
     grid.Foreach_3d(0, 0, [&](int i, int j, int k) {
 #if 0
-	mprintf("[%d, %d, %d] = %06g %06g %06g\n", i, j, k,
-		(double) mflds[p](EX, i, j, k), (double) mflds[p](EY, i, j, k), (double) mflds[p](EZ, i, j, k));
+	mprintf("p%d [%d, %d, %d] = %06g %06g %06g\n", p, i, j, k,
+		(double) mflds2[p](EX, i, j, k), (double) mflds2[p](EY, i, j, k), (double) mflds2[p](EZ, i, j, k));
 #endif
+#if 1
       for (int m = 0; m < NR_FIELDS; m++) {
         EXPECT_EQ(mflds[p](m, i, j, k), mflds2[p](m, i, j, k))
           << " i " << i << " j " << j << " k " << k << " m " << m;
       }
+#endif
     });
   }
 }
