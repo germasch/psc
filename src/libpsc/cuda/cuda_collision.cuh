8ab9e387c5a85f693d2757d58333ca2b4b766ba5
Kai Germaschewski <kai.germaschewski@unh.edu>
2020-06-07 12:29:22 -0400

    start k_collide2

--- src/libpsc/cuda/cuda_collision.cuh
+++ src/libpsc/cuda/cuda_collision.cuh
@@ -25,6 +25,48 @@ __global__ static void k_collide(
     dmprts, d_off, d_id, nudt0, rng_state, n_cells, n_cells_per_patch);
 }
 
+template <typename cuda_mparticles, typename RngState>
+__global__ static void k_collide2(
+  DMparticlesCuda<typename cuda_mparticles::BS> dmprts, uint* d_off, uint* d_id,
+  float nudt0, typename RngState::Device rng_state, uint n_cells, uint n_cells_per_patch)
+{
+  using real_t = typename cuda_mparticles::real_t;
+  using DMparticles = DMparticlesCuda<typename cuda_mparticles::BS>;
+  using DParticle = DParticleProxy<DMparticles>;
+
+  int id = threadIdx.x + blockIdx.x * THREADS_PER_BLOCK;
+  /* Copy state to local memory for efficiency */
+  auto rng = rng_state[id];
+  BinaryCollision<DParticle> bc;
+  
+  for (uint bidx = blockIdx.x; bidx < n_cells; bidx += gridDim.x) {
+    uint beg = d_off[bidx];
+    uint end = d_off[bidx + 1];
+    real_t nudt1 =
+      nudt0 * (end - beg) * (end - beg) / ((end - beg) & ~1); // somewhat counteract that we don't collide
+    // the last particle if odd
+    for (uint n = beg + 2 * threadIdx.x; n + 1 < end;
+	 n += 2 * THREADS_PER_BLOCK) {
+      // printf("%d/%d: n = %d off %d\n", blockIdx.x, threadIdx.x, n,
+      // d_off[blockIdx.x]);
+      auto prt1 = DParticle{dmprts.storage.load_proxy(dmprts, d_id[n])};
+      auto prt2 = DParticle{dmprts.storage.load_proxy(dmprts, d_id[n + 1])};
+#ifndef NDEBUG
+      int p = bidx / n_cells_per_patch;
+      int cidx1 = dmprts.validCellIndex(dmprts.storage.xi4[d_id[n]], p);
+      int cidx2 = dmprts.validCellIndex(dmprts.storage.xi4[d_id[n + 1]], p);
+      assert(cidx1 == cidx2);
+#endif
+      bc(prt1, prt2, nudt1, rng);
+      // xi4 is not modified, don't need to store
+      dmprts.storage.store_momentum(prt1, d_id[n]);
+      dmprts.storage.store_momentum(prt2, d_id[n + 1]);
+    }
+  }
+  
+  rng_state[id] = rng;
+}
+
 // ======================================================================
 // cuda_collision
 
@@ -70,10 +112,17 @@ struct CudaCollision
                      // this //prts[n_start].w());
     real_t nudt0 = wni / nicell_ * interval_ * dt_ * nu_;
 
+#if 0
     k_collide<cuda_mparticles, RngState><<<dimGrid, THREADS_PER_BLOCK>>>(
       cmprts, sort_.d_off.data().get(), sort_.d_id.data().get(), nudt0,
       rng_state_, cmprts.n_cells(), n_cells_per_patch);
     cuda_sync_if_enabled();
+#else
+    k_collide2<cuda_mparticles, RngState><<<dimGrid, THREADS_PER_BLOCK>>>(
+      cmprts, sort_.d_off.data().get(), sort_.d_id.data().get(), nudt0,
+      rng_state_, cmprts.n_cells(), n_cells_per_patch);
+    cuda_sync_if_enabled();
+#endif
   }
 
   __device__ static void d_collide(DMparticles dmprts, uint* d_off, uint* d_id,
