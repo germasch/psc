ca2e3e68fd69dac2ae1c612f91e6140f782edd65
Kai Germaschewski <kai.germaschewski@unh.edu>
2020-06-08 23:12:52 -0400

    cuda: share current deposition

--- src/libpsc/psc_push_particles/inc_curr_1vb_split.c
+++ src/libpsc/psc_push_particles/inc_curr_1vb_split.c
@@ -104,23 +104,15 @@ struct Current1vbSplit
       xa[d] -= i[d];
     }
 
+    int ci0[3] = {};
     real_t fnqx = qni_wni * fnqxs_;
-    curr_cache.add(0, i[0]  ,i[1]  ,i[2]  , fnqx * (dx[0] * (1.f - xa[1]) * (1.f - xa[2]) + h));
-    curr_cache.add(0, i[0]  ,i[1]+1,i[2]  , fnqx * (dx[0] * (      xa[1]) * (1.f - xa[2]) - h));
-    curr_cache.add(0, i[0]  ,i[1]  ,i[2]+1, fnqx * (dx[0] * (1.f - xa[1]) * (      xa[2]) - h));
-    curr_cache.add(0, i[0]  ,i[1]+1,i[2]+1, fnqx * (dx[0] * (      xa[1]) * (      xa[2]) + h));
+    currx(curr_cache, i, fnqx, dx, xa, h, ci0);
 
     real_t fnqy = qni_wni * fnqys_;
-    curr_cache.add(1, i[0]  ,i[1]  ,i[2]  , fnqy * (dx[1] * (1.f - xa[0]) * (1.f - xa[2]) + h));
-    curr_cache.add(1, i[0]+1,i[1]  ,i[2]  , fnqy * (dx[1] * (      xa[0]) * (1.f - xa[2]) - h));
-    curr_cache.add(1, i[0]  ,i[1]  ,i[2]+1, fnqy * (dx[1] * (1.f - xa[0]) * (      xa[2]) - h));
-    curr_cache.add(1, i[0]+1,i[1]  ,i[2]+1, fnqy * (dx[1] * (      xa[0]) * (      xa[2]) + h));
+    curry(curr_cache, i, fnqy, dx, xa, h, ci0);
 
     real_t fnqz = qni_wni * fnqzs_;
-    curr_cache.add(2, i[0]  ,i[1]  ,i[2]  , fnqz * (dx[2] * (1.f - xa[0]) * (1.f - xa[1]) + h));
-    curr_cache.add(2, i[0]+1,i[1]  ,i[2]  , fnqz * (dx[2] * (      xa[0]) * (1.f - xa[1]) - h));
-    curr_cache.add(2, i[0]  ,i[1]+1,i[2]  , fnqz * (dx[2] * (1.f - xa[0]) * (      xa[1]) - h));
-    curr_cache.add(2, i[0]+1,i[1]+1,i[2]  , fnqz * (dx[2] * (      xa[0]) * (      xa[1]) + h));
+    currz(curr_cache, i, fnqz, dx, xa, h, ci0);
   }
 
   void calc_j2_one_cell(fields_t curr_cache, real_t qni_wni,
