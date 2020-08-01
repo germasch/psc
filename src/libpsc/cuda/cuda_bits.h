7cd84ef697f1dccaa99c2d38be989ebf5fa42b57
Kai Germaschewski <kai.germaschewski@unh.edu>
2020-08-01 08:18:39 -0400

    psc::device_vector

--- src/libpsc/cuda/cuda_bits.h
+++ src/libpsc/cuda/cuda_bits.h
@@ -2,18 +2,27 @@
 #ifndef CUDA_BITS_H
 #define CUDA_BITS_H
 
+#ifdef HAVE_RMM
+
+#include <rmm/thrust_rmm_allocator.h>
+
+namespace psc
+{
+template <typename T>
+using device_vector = rmm::device_vector<T>;
+}
+
+#else // !HAVE_RMM
 
 #include <thrust/device_vector.h>
-#ifdef HAVE_RMM
-#include "rmm/thrust_rmm_allocator.h"
 
-template <typename T>
-using device_vector = thrust::device_vector<T, rmm::mr::thrust_allocator<T>>;
-#else
+namespace psc
+{
 template <typename T>
 using device_vector = thrust::device_vector<T>;
-#endif
+}
 
+#endif
 
 #define cudaCheck(ierr)                                                        \
   do {                                                                         \
