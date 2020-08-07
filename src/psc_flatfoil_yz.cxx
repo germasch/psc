c6d3e296125e761ce50abf2394809276783eb0be
Kai Germaschewski <kai.germaschewski@unh.edu>
2020-08-06 21:01:17 -0400

    again

--- src/psc_flatfoil_yz.cxx
+++ src/psc_flatfoil_yz.cxx
@@ -525,8 +525,8 @@ int main(int argc, char** argv)
   return 0;
 }
 
-void init_npt_zero(int kind, Vec3<double> pos, int p, Int3 idx,
-		   psc_particle_npt& npt);
+void init_npt_zero(int kind, Double3 pos, int p, Int3 idx,
+		   psc_particle_npt& npt)
 {
   npt.n = 0;
 }
