4a06902782217be4175cf1b767bc2be996fe1937
Kai Germaschewski <kai.germaschewski@unh.edu>
2020-10-08 14:27:01 -0400

    load dbg

--- src/libpsc/psc_balance/psc_balance_impl.hxx
+++ src/libpsc/psc_balance/psc_balance_impl.hxx
@@ -377,7 +377,7 @@ private:
       if (factor_fields_ >= 0.) {
 	const int *ldims = grid.ldims;
 	load = n_prts_by_patch[p] + factor_fields_ * ldims[0] * ldims[1] * ldims[2];
-	//mprintf("loads p %d %g %g ratio %g\n", p, loads[p], comp_time, loads[p] / comp_time);
+	printf("loads p %d load %g prts %d\n", p, loads[p], n_prts_by_patch[p]);
       } else {
 	load = psc_balance_comp_time_by_patch[p];
       }
