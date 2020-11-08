c567aaa2e7249c292fdb155d91ce3996c3e890a5
Kai Germaschewski <kai.germaschewski@unh.edu>
2020-11-07 22:50:28 -0500

    nmax 40001

--- src/psc_flatfoil_yz.cxx
+++ src/psc_flatfoil_yz.cxx
@@ -247,7 +247,7 @@ HMFields make_MfieldsMoment_n<MfieldsCuda>(const Grid_t& grid)
 void setupParameters()
 {
   // -- set some generic PSC parameters
-  psc_params.nmax = 20001; // 5001;
+  psc_params.nmax = 40001; // 5001;
   psc_params.cfl = 0.75;
   psc_params.write_checkpoint_every_step = 2000;
   psc_params.stats_every = 1;
