e5a53aa14ba860fe8a35325e47153e63002c4e80
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-03-21 10:36:13 -0400

    TMP: freq output

--- src/psc_flatfoil_yz.cxx
+++ src/psc_flatfoil_yz.cxx
@@ -510,13 +510,13 @@ void run()
   outf_item_params.pfield_interval = 100;
   outf_item_params.tfield_interval = -100;
 #elif CASE == CASE_2D_SMALL
-  outf_item_params.pfield_interval = 100;
-  outf_item_params.tfield_interval = -500;
+  outf_item_params.pfield_interval = 4;
+  outf_item_params.tfield_interval = 4;
 #else
   outf_item_params.pfield_interval = 500;
   outf_item_params.tfield_interval = 500;
 #endif
-  outf_item_params.tfield_average_every = 50;
+  outf_item_params.tfield_average_every = 2;
 
   outf_params.fields = outf_item_params;
   outf_params.moments = outf_item_params;
