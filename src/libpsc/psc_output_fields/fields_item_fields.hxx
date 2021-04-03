c2e1e5e2fe9cedef7c5a3c3382a7cc61726dcd9a
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-04-02 22:02:09 -0400

    gt: remove an unused version of evalMfields

--- src/libpsc/psc_output_fields/fields_item_fields.hxx
+++ src/libpsc/psc_output_fields/fields_item_fields.hxx
@@ -60,11 +60,6 @@ inline MfieldsC evalMfields(const MfieldsSingle& _exp)
   return mflds;
 }
 
-inline const MfieldsC& evalMfields(const MfieldsC& mflds)
-{
-  return mflds;
-}
-
 #ifdef USE_CUDA
 
 template <typename E,
