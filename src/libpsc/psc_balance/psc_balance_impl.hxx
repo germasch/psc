1d5cdba15b2ade4cbb55c6ff0f59310d99529f80
Kai Germaschewski <kai.germaschewski@unh.edu>
2021-06-09 09:30:32 -0400

    bnd: FIXME very hacky clearing of bnd maps

--- src/libpsc/psc_balance/psc_balance_impl.hxx
+++ src/libpsc/psc_balance/psc_balance_impl.hxx
@@ -10,6 +10,8 @@
 
 #ifdef USE_CUDA
 #include "psc_fields_cuda.h"
+#include "mparticles_cuda.hxx"
+#include "bnd_cuda_3_impl.hxx"
 #endif
 
 #include <mrc_profile.h>
@@ -964,7 +966,11 @@ private:
       p_mp_host = new Mparticles{mp_base.grid()};
       auto& mp_host = *p_mp_host;
       MparticlesBase::convert(mp_base, mp_host);
+#ifdef USE_CUDA
+      mp_base.~MparticlesBase();
+#else
       mp_base.reset(*new_grid); // frees memory here already
+#endif
     }
 
 #ifdef USE_CUDA
@@ -1001,6 +1007,9 @@ private:
       MfieldsBase::convert(mf_cuda, mf_old, 0, mf_cuda.n_comps());
       mf_cuda.~MfieldsCuda();
     }
+    MEM_STATS();
+    BndCuda3<MfieldsCuda>::clear();
+    BndCuda3<MfieldsStateCuda>::clear();
 #endif
     MEM_STATS();
 
@@ -1073,12 +1082,16 @@ private:
 
     // mv particles back to gpu
     if (mp && typeid(*mp) != typeid(Mparticles)) {
+#ifdef USE_CUDA
+      new (mp) MparticlesCuda<BS444>{*new_grid};
       auto& mp_base = *mp;
+
       mpi_printf(old_grid->comm(), "***** Balance: particles to device\n");
       assert(p_mp_host);
       auto& mp_host = *p_mp_host;
       MparticlesBase::convert(mp_host, mp_base);
       delete p_mp_host;
+#endif
     }
 
     // update psc etc
