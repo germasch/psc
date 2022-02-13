f698793eef16a5ec17cb0131e409a60a0e15390c
Kai Germaschewski <kai.germaschewski@unh.edu>
2022-02-12 23:29:16 -0500

    wip: flatfoil/small: test

--- src/psc_flatfoil_yz.cxx
+++ src/psc_flatfoil_yz.cxx
@@ -236,7 +236,7 @@ using Heating = typename HeatingSelector<Mparticles>::Heating;
 void setupParameters()
 {
   // -- set some generic PSC parameters
-  psc_params.nmax = 10; // 5001;
+  psc_params.nmax = 11; // 5001;
   psc_params.cfl = 0.75;
   psc_params.write_checkpoint_every_step = -1000;
   psc_params.stats_every = 1;
@@ -474,7 +474,7 @@ void run()
   // -- Checks
   ChecksParams checks_params{};
 #if CASE == CASE_2D_SMALL
-  checks_params.continuity_every_step = 1;
+  checks_params.continuity_every_step = -1;
   checks_params.continuity_dump_always = true;
 #else
   checks_params.continuity_every_step = 0;
@@ -484,7 +484,7 @@ void run()
   checks_params.continuity_verbose = true;
 
 #if CASE == CASE_2D_SMALL
-  checks_params.gauss_every_step = 1;
+  checks_params.gauss_every_step = -1;
   checks_params.gauss_dump_always = true;
 #else
   checks_params.gauss_every_step = 100;
@@ -500,7 +500,7 @@ void run()
   int marder_loop = 3;
 #if CASE == CASE_2D_SMALL
   bool marder_dump = true;
-  psc_params.marder_interval = 2;
+  psc_params.marder_interval = -2;
 #else
   bool marder_dump = false;
   psc_params.marder_interval = 100;
@@ -519,8 +519,8 @@ void run()
   outf_item_params.pfield_interval = 100;
   outf_item_params.tfield_interval = -100;
 #elif CASE == CASE_2D_SMALL
-  outf_item_params.pfield_interval = 4;
-  outf_item_params.tfield_interval = 4;
+  outf_item_params.pfield_interval = -10;
+  outf_item_params.tfield_interval = -4;
 #else
   outf_item_params.pfield_interval = 500;
   outf_item_params.tfield_interval = 500;
