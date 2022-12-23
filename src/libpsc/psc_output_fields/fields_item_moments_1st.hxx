10d6dfd84ae68d0e3e97aafe40d82e757021545f
Kai Germaschewski <kai.germaschewski@unh.edu>
2022-12-22 21:20:10 -0500

    fixup mres_gt_/ib_

--- src/libpsc/psc_output_fields/fields_item_moments_1st.hxx
+++ src/libpsc/psc_output_fields/fields_item_moments_1st.hxx
@@ -304,7 +304,7 @@ public:
     using real_t = R;
 
     psc::moment::deposit_1st_cc<dim_t>(
-      Base::mres_.storage(), Base::mres_.ib(), h_mprts,
+      Base::mres_gt_, Base::mres_ib_, h_mprts,
       [&](auto& deposit_one, const auto& prt) {
         int mm = prt.kind() * n_moments;
         real_t vxi[3];
